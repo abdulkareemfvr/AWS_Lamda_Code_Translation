08111b801532372f909df4b5fddd9535faa421f7
abdulkareemfvr <abdulkareemfvr@gmail.com>
2023-05-22 15:12:46 +0500

    lambda function code pushed

---
+++ lamda_function_to_translate_the_images_and_text.py
@@ -0,0 +1,51 @@
+import boto3
+import json
+
+
+s3_client = boto3.client('s3')
+rekognition_client = boto3.client('rekognition')
+translate_client = boto3.client('translate')
+
+
+def translate_text(text, lang_code):
+    result = translate_client.translate_text(
+        Text=text,
+        SourceLanguageCode='auto',
+        TargetLanguageCode=lang_code
+    )
+    return result['TranslatedText']
+
+
+def lambda_handler(event, context):
+
+    input_bucket = event['Records'][0]['s3']['bucket']['name']
+    input_key = event['Records'][0]['s3']['object']['key']
+
+    # check if input file is an image file
+    if input_key.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+  
+        response = rekognition_client.detect_text(Image={'S3Object':{'Bucket':input_bucket,'Name':input_key}})
+        recognized_text = ''
+        for text in response['TextDetections']:
+            if text['Type'] == 'LINE':
+                recognized_text += text['DetectedText'] + '\n'
+    else:
+ 
+        response = s3_client.get_object(Bucket=input_bucket, Key=input_key) 
+        recognized_text = ''
+        for line in response["Body"].read().splitlines():
+            each_line = line.decode('utf-8')
+            if(each_line!=''):
+                recognized_text += each_line + '\n'
+
+   
+    translated_text = translate_text(recognized_text, 'de')
+
+
+    output_key = input_key + '.txt'
+    s3_client.put_object(Body=translated_text, Bucket='sai-outputdocument', Key=output_key)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Translation complete!')
+    }
